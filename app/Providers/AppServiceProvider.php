a92dba63d0e80f7255e883f002fde479dc446242
Fanza atsila Fizarli <fanzaatsilafizarli@MacBook-Air-Fanza.local>
2026-01-05 10:31:26 +0700

    revisi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Force HTTPS for URLs
-        config(['app.url' => 'http://localhost:8000']);
-        URL::forceScheme('http');
-        $this->app['url']->forceRootUrl('http://localhost:8000');
+        config(['app.url' => 'https://udberkahaa.com']);
+        URL::forceScheme('https');
+        $this->app['url']->forceRootUrl('https://udberkahaa.com');
         
         // Implicitly grant "Super Admin" role all permissions
         // This works in the app by using gate-related functions like auth()->user->can() and @can()
